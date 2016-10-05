7b3f2582e784b50f86ddc2107a8a47f2388cc5e9
jirik <jirik@users.noreply.github.com>
2016-10-05 13:04:47 +0200

    remove non-munimap examples

--- src/client/example/module/indexcfg.modaware.modon.js
+++
@@ -1,6 +0,0 @@
-goog.provide('example.module.indexcfg.modaware');
-
-/**
- * @fileoverview If the app is compiled with modules ENABLED, then we need to
- * set notnihg (because goog.module.* will do it).
- */
