6248dcb17f696e231c8234f63e6ef0a58c16b27d
jirik <jirik@users.noreply.github.com>
2016-10-13 09:50:39 +0200

    remove forgotten console.log

--- src/client/munimaplib.js
+++ src/client/munimaplib.js
@@ -4,8 +4,6 @@ goog.require('munimap.create');
 goog.require('munimap.reset');
 goog.require('polyfill.es6');
 
-console.log(ol.Feature);
-console.log(ol.render.Feature);
 
 goog.exportSymbol('munimap.create', munimap.create);
 goog.exportSymbol('munimap.reset', munimap.reset);
