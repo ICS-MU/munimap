7a5ef438f00d3ba43c1c2d07458fcb9675016fe9
Andrea Kýnová <kynova@ics.muni.cz>
2016-11-21 16:15:45 +0100

    classroom label consists of number (if not included in room name) and Inet room name

--- src/client/munimap/room.style.js
+++ src/client/munimap/room.style.js
@@ -250,11 +250,22 @@ munimap.room.style.labelFunction = function(options, feature, resolution) {
         }
         var title;
         var fTitle = feature.get(munimap.lang.getMsg(
-            munimap.lang.Translations.ROOM_TITLE_FIELD_NAME)) ||
-                feature.get('cislo');
-        if (fTitle) {
-          title = goog.asserts.assertString(fTitle);
-          title = munimap.room.style.alignRoomTitleToRows(title);
+            munimap.lang.Translations.ROOM_TITLE_FIELD_NAME));
+        var fNumber = feature.get('cislo');
+        if (fTitle || fNumber) {
+          if (fTitle) {
+            title = goog.asserts.assertString(fTitle);
+            title = munimap.room.style.alignRoomTitleToRows(title);
+            if (fNumber) {
+              var re =
+                  new RegExp('(^| )' + fNumber.toLowerCase() + '( |$)', 'g');
+              if (!re.test(fTitle.toLowerCase())) {
+                title = fNumber + '\n' + title;
+              }
+            }
+          } else {
+            title = goog.asserts.assertString(fNumber);
+          }
           var purpose = feature.get('ucel_gis');
           var fontSize;
           if (munimap.range.contains(
