337196e8e373b82c0f0b7d816cd7b1e87fd29419
Andrea Kýnová <kynova@ics.muni.cz>
2016-11-24 18:33:47 +0100

    fix of marked room label - change of getDefaultLabel method of room

--- src/client/munimap/room.style.js
+++ src/client/munimap/room.style.js
@@ -248,24 +248,9 @@ munimap.room.style.labelFunction = function(options, feature, resolution) {
         if (labelCache[uid]) {
           return labelCache[uid];
         }
-        var title;
-        var fTitle = feature.get(munimap.lang.getMsg(
-            munimap.lang.Translations.ROOM_TITLE_FIELD_NAME));
-        var fNumber = feature.get('cislo');
-        if (fTitle || fNumber) {
-          if (fTitle) {
-            title = goog.asserts.assertString(fTitle);
-            title = munimap.room.style.alignRoomTitleToRows(title);
-            if (fNumber) {
-              var re =
-                  new RegExp('(^| )' + fNumber.toLowerCase() + '( |$)', 'g');
-              if (!re.test(fTitle.toLowerCase())) {
-                title = fNumber + '\n' + title;
-              }
-            }
-          } else {
-            title = goog.asserts.assertString(fNumber);
-          }
+        goog.asserts.assertInstanceof(feature, ol.Feature);
+        var title = munimap.room.getDefaultLabel(feature);
+        if (title) {
           var purpose = feature.get('ucel_gis');
           var fontSize;
           if (munimap.range.contains(
