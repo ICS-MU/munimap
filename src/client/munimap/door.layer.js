e1c9558f30f58c6e5e0d80352354417fdd282a7a
Andrea Kýnová <kynova@ics.muni.cz>
2017-02-24 13:33:27 +0100

    preparation for custom layer - styleFragments implemented for door

--- src/client/munimap/door.layer.js
+++ src/client/munimap/door.layer.js
@@ -7,10 +7,19 @@ goog.require('munimap.door');
  * @return {ol.layer.Vector}
  */
 munimap.door.layer.create = function() {
+  var styleFragments = {
+    'activeFloorFeature': {
+      filter: goog.functions.TRUE,
+      style: munimap.door.STYLE
+    }
+  };
+
   return new ol.layer.Vector({
     id: munimap.door.ACTIVE_LAYER_ID,
     'clearSourceOnFloorChange': true,
     'type': munimap.door.TYPE,
+    'refreshStyleOnFloorChange': true,
+    'styleFragments': styleFragments,
     maxResolution: munimap.door.RESOLUTION.max,
     source: null,
     updateWhileAnimating: true,
