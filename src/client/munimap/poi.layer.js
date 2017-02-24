a571c7c8656a23f5d8fd7c9c70bdec5895090423
Andrea Kýnová <kynova@ics.muni.cz>
2017-02-24 12:21:04 +0100

    preparation for custom layer - styleFragments implemented for poi

--- src/client/munimap/poi.layer.js
+++ src/client/munimap/poi.layer.js
@@ -7,12 +7,29 @@ goog.require('munimap.poi');
  * @return {ol.layer.Vector}
  */
 munimap.poi.layer.create = function() {
+  var styleFragments = {
+    'activeFloorFeature': {
+      filter: munimap.poi.style.activeFloorFilter,
+      style: munimap.poi.style.activeFloorFunction
+    },
+    'outdoorFeature': {
+      filter: munimap.poi.style.outdoorFilter,
+      style: munimap.poi.style.outdoorFunction
+    },
+    'defaultFloorFeature': {
+      filter: munimap.poi.style.defaultFloorFilter,
+      style: munimap.poi.style.defaultFunction
+    }
+  };
+
   return new ol.layer.Vector({
     id: munimap.poi.ACTIVE_LAYER_ID,
     'isFeatureClickable': munimap.poi.isClickable,
     'featureClickHandler': munimap.poi.featureClickHandler,
     'clearSourceOnFloorChange': true,
     'type': munimap.poi.TYPE,
+    'refreshStyleOnFloorChange': true,
+    'styleFragments': styleFragments,
     maxResolution: munimap.poi.RESOLUTION.max,
     source: null,
     updateWhileAnimating: true,
