15718292e11ab32aaa69bf379023b7f617b03896
Pavel Bohumel <bohumel@ics.muni.cz>
2017-03-06 17:00:27 +0100

    Fix path to SDE connection file

--- resources/python/src/config.py
+++ resources/python/src/config.py
@@ -1,2 +1,2 @@
-gisdb = "python/connection/gisdb2.sde"
-gisdb_munimap = "python/connection/gisdb2_munimap.sde"
+gisdb = "..\connection\gisdb2.sde"
+gisdb_munimap = "..\connection\gisdb2_munimap.sde"
