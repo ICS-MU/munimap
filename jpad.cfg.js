142b318c3ec1c04133eaa73f6fff1dde350ffd59
Petr Kovács <kovacs@ics.muni.cz>
2017-04-21 17:16:49 +0200

    version 1.2.2

--- jpad.cfg.js
+++ jpad.cfg.js
@@ -2,7 +2,7 @@
 
 var jpad = {
   appPath: '/munimap/',
-  appVersion: '1.2.1',
+  appVersion: '1.2.2',
   olVersion: 'v3.19.1',
   prodDomain: 'maps.muni.cz',
   plovrPattern: 'src/client/**/*.plovr.json',
