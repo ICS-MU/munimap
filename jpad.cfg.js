bf0276f85a406f81cc5253f0bcc7ca4ec11a6293
Jan Sikora <etomanon@gmail.com>
2018-03-26 10:23:32 +0200

    version 1.6.4

--- jpad.cfg.js
+++ jpad.cfg.js
@@ -2,7 +2,7 @@
 
 var jpad = {
   appPath: '/munimap/',
-  appVersion: '1.6.3',
+  appVersion: '1.6.4',
   olVersion: 'v4.6.4',
   prodDomain: 'maps.muni.cz',
   plovrPattern: 'src/client/**/*.plovr.json',
