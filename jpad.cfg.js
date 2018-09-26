f8ebb5ef56242ba866e4bbf94207b8c183d5fee1
Jan Sikora <436986@muni.cz>
2018-09-26 12:50:20 +0200

    version 1.8.2

--- jpad.cfg.js
+++ jpad.cfg.js
@@ -2,7 +2,7 @@
 
 var jpad = {
   appPath: '/munimap/',
-  appVersion: '1.8.1',
+  appVersion: '1.8.2',
   olVersion: 'v4.6.4',
   prodDomain: 'maps.muni.cz',
   plovrPattern: 'src/client/**/*.plovr.json',
