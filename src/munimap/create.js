9bfad4fa2991b34918812184d5c1f4f838d49697
Jiri Kozel <jirik@users.noreply.github.com>
2021-04-01 21:37:00 +0200

    Add typescript for typechecking

--- src/munimap/create.js
+++ src/munimap/create.js
@@ -4,6 +4,25 @@ import TileLayer from 'ol/layer/Tile';
 import {Map, View} from 'ol';
 import {fromLonLat} from 'ol/proj';
 
+/**
+ * @typedef {import("ol/coordinate").Coordinate} ol.coordinate.Coordinate
+ * @typedef {import("ol/layer").Vector} ol.layer.Vector
+ * @typedef {import("ol").Feature} ol.Feature
+ */
+
+/**
+ * @typedef {Object} Options
+ * @property {string} target
+ * @property {number} [zoom]
+ * @property {ol.coordinate.Coordinate} [center]
+ * @property {Array.<string>|string} [zoomTo]
+ * @property {Array.<string>|Array.<ol.Feature>} [markers]
+ */
+
+/**
+ * @param {Options} opts Options
+ * @returns {Map} initialized map
+ */
 export default (opts) => {
   console.log('munimap.create', opts);
   return new Map({
--- src/munimap/reset.js
+++ src/munimap/reset.js
@@ -1,4 +1,8 @@
 /* eslint-disable no-console */
+
+/**
+ * @param {Object} opts Options
+ */
 export default (opts) => {
   console.log('munimap.reset', opts);
 };
