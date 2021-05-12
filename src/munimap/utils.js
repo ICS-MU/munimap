402a3d1e5c035f3d02e79e080942ba638564d013
Jan Tajovský <423920@mail.muni.cz>
2021-05-12 21:47:50 +0200

    added basic functions to utils

--- src/munimap/utils.js
+++ src/munimap/utils.js
@@ -10,8 +10,67 @@ const removeArrayDuplicates = (arr) => {
   arr = [...new Set(arr)];
 };
 
+/**
+ * @param {?} val value
+ * @return {boolean} isString
+ */
 const isString = (val) => {
   return typeof val === 'string';
 };
 
-export {removeArrayDuplicates, isString};
+/**
+ * Returns true if the specified value is a number.
+ * @param {?} val Variable to test.
+ * @return {boolean} Whether variable is a number.
+ */
+const isNumber = (val) => {
+  return typeof val === 'number';
+};
+
+/**
+ * Returns true if the specified value is not undefined.
+ *
+ * @param {?} val Variable to test.
+ * @return {boolean} Whether variable is defined.
+ */
+const isDef = (val) => {
+  return val !== undefined;
+};
+
+/**
+ * Returns true if the specified value is defined and not null.
+ * @param {?} val Variable to test.
+ * @return {boolean} Whether variable is defined and not null.
+ */
+const isDefAndNotNull = (val) => {
+  //undefined == null.
+  return val != null;
+};
+
+/**
+ * Returns true if the specified value is an array.
+ * @param {?} val Variable to test.
+ * @return {boolean} Whether variable is an array.
+ */
+const isArray = (val) => {
+  return Array.isArray(val);
+};
+
+/**
+ * Returns true if the specified value is a boolean.
+ * @param {?} val Variable to test.
+ * @return {boolean} Whether variable is boolean.
+ */
+const isBoolean = (val) => {
+  return typeof val === 'boolean';
+};
+
+export {
+  removeArrayDuplicates,
+  isString,
+  isNumber,
+  isDef,
+  isDefAndNotNull,
+  isArray,
+  isBoolean,
+};
