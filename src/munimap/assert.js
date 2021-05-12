abab3fe7df2ebc19ae91fb01b9f3a54e11947bef
Jan Tajovský <423920@mail.muni.cz>
2021-05-12 21:23:37 +0200

    changed assert function

--- src/munimap/assert.js
+++ src/munimap/assert.js
@@ -1,9 +1,12 @@
 /**
  * @param {*} assertion Assertion we expected to be truthy.
  * @param {string=} opt_msg optional message
+ * @return {boolean} assertion
+ * @throws {AssertionError} AssertionError
  */
 export default (assertion, opt_msg) => {
   if (!assertion) {
     throw new Error(opt_msg);
   }
+  return assertion;
 };
