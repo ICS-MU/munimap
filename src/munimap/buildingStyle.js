f1670956c9d2006af052c6448c9f2c66b47bd0ae
Jan Tajovský <423920@mail.muni.cz>
2021-06-22 13:10:30 +0200

    added constants for building style

---
+++ src/munimap/buildingStyle.js
@@ -0,0 +1,15 @@
+/**
+ * @module buildingStyle
+ */
+
+/**
+ * @type {number}
+ */
+const FONT_SIZE = 13;
+
+/**
+ * @type {number}
+ */
+const BIG_FONT_SIZE = 15;
+
+export {FONT_SIZE, BIG_FONT_SIZE};
