fb3ca35485ec8ae2679a9550090161b7f57a8b5c
Jan Tajovský <423920@mail.muni.cz>
2021-06-22 13:03:47 +0200

    added layer prop names to constants

---
+++ src/munimap/layer.js
@@ -0,0 +1,55 @@
+/**
+ * @module layer
+ */
+
+/**
+ * @type {string}
+ * @const
+ */
+const CLICK_HANDLER = 'featureClickHandler';
+
+/**
+ * @type {string}
+ * @const
+ */
+const IS_CLICKABLE = 'isFeatureClickable';
+
+/**
+ * @type {string}
+ * @const
+ */
+const CLEAR_SOURCE = 'clearSourceOnFloorChange';
+
+/**
+ * @type {string}
+ * @const
+ */
+const REDRAW = 'redrawOnFloorChange';
+
+/**
+ * @type {string}
+ * @const
+ */
+const REFRESH_STYLE = 'refreshStyleOnFloorChange';
+
+/**
+ * @type {string}
+ * @const
+ */
+const STYLE_FRAGMENTS = 'styleFragments';
+
+/**
+ * @type {string}
+ * @const
+ */
+const TYPE = 'type';
+
+export {
+  CLICK_HANDLER,
+  IS_CLICKABLE,
+  CLEAR_SOURCE,
+  REDRAW,
+  REFRESH_STYLE,
+  STYLE_FRAGMENTS,
+  TYPE,
+};
