82e7324bd18a86ce5ecca7de065d5bd3a14dba99
Jan Tajovský <423920@mail.muni.cz>
2021-09-07 13:06:21 +0200

    added new scripts for building munimap to different folders; automatic hiding of warning wrapper

--- webpack.config.dev.babel.js
+++ webpack.config.dev.babel.js
@@ -5,147 +5,148 @@ import glob from 'glob';
 import path from 'path';
 import webpack from 'webpack';
 
-const APP_PATH = '/munimap/testing/';
-const PROD_DOMAIN = `localhost:${JSON.stringify(8080)}`;
+export default (env) => {
+  const APP_PATH = '/munimap/testing/';
+  const PROD_DOMAIN = `localhost:8080`;
 
-const OUTPUT_PATH = path.join(path.resolve(__dirname, 'dist'), APP_PATH);
+  const OUTPUT_PATH = path.join(path.resolve(__dirname, 'dist'), APP_PATH);
 
-const opts = {
-  scriptLoading: 'blocking',
-  inject: 'head',
-  minify: false,
-  appVersion: PACKAGE.version,
-  olVersion: PACKAGE.dependencies.ol.substr(0, 1).match(/[0-9]/i)
-    ? PACKAGE.dependencies.ol
-    : PACKAGE.dependencies.ol.substring(1),
-  appPath: APP_PATH,
-  prodDomain: PROD_DOMAIN,
-};
-const examplePageNames = glob
-  .sync('./src/example/*.html')
-  .map((item) => path.basename(item, '.html'));
-const exampleHtmlPlugins = examplePageNames.map((name) => {
-  return new HtmlWebpackPlugin({
-    template: `./src/example/${name}.html`,
-    filename: `./example/${name}.html`,
-    ...opts,
-  });
-});
-
-const preprocessor = (content) => {
-  const newContent = (content) => {
-    const INCLUDE_PATTERN = /<%= (.*) %>/gi;
-    return INCLUDE_PATTERN.test(content)
-      ? content.replace(INCLUDE_PATTERN, (_, group) => opts[group])
-      : content;
+  const opts = {
+    scriptLoading: 'blocking',
+    inject: 'head',
+    minify: false,
+    appVersion: PACKAGE.version,
+    olVersion: PACKAGE.dependencies.ol.substr(0, 1).match(/[0-9]/i)
+      ? PACKAGE.dependencies.ol
+      : PACKAGE.dependencies.ol.substring(1),
+    appPath: APP_PATH,
+    prodDomain: PROD_DOMAIN,
+    warningWrapperClass: 'hide',
   };
-  return newContent(content);
-};
+  const examplePageNames = glob
+    .sync('./src/example/*.html')
+    .map((item) => path.basename(item, '.html'));
+  const exampleHtmlPlugins = examplePageNames.map((name) => {
+    return new HtmlWebpackPlugin({
+      template: `./src/example/${name}.html`,
+      filename: `./example/${name}.html`,
+      ...opts,
+    });
+  });
 
-export default {
-  entry: {
-    munimaplib: [
-      'regenerator-runtime/runtime',
-      path.resolve(__dirname, 'src/munimap/index.js'),
-    ],
-  },
-  output: {
-    //path: path.resolve(__dirname, 'dist'),
-    path: OUTPUT_PATH,
-    filename: '[name].js',
-    library: {
-      name: 'munimap',
-      type: 'umd',
+  const preprocessor = (content) => {
+    const newContent = (content) => {
+      const INCLUDE_PATTERN = /<%= (.*) %>/gi;
+      return INCLUDE_PATTERN.test(content)
+        ? content.replace(INCLUDE_PATTERN, (_, group) => opts[group])
+        : content;
+    };
+    return newContent(content);
+  };
+
+  return {
+    entry: {
+      munimaplib: [
+        'regenerator-runtime/runtime',
+        path.resolve(__dirname, 'src/munimap/index.js'),
+      ],
     },
-    clean: true,
-  },
-  devServer: {
-    //static: path.join(__dirname, 'dist'),
-    static: OUTPUT_PATH,
-    hot: true,
-    open: [APP_PATH],
-    dev: {
-      publicPath: APP_PATH,
+    output: {
+      path: OUTPUT_PATH,
+      filename: '[name].js',
+      library: {
+        name: 'munimap',
+        type: 'umd',
+      },
+      clean: true,
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: {
-              minimize: false,
-              preprocessor: preprocessor,
-            },
-          },
-        ],
+    devServer: {
+      static: OUTPUT_PATH,
+      hot: true,
+      open: [APP_PATH],
+      dev: {
+        publicPath: APP_PATH,
       },
-      {
-        test: /\.css$/,
-        exclude: [
-          path.resolve(__dirname, 'src', 'css', 'example'),
-          path.join(__dirname, 'src', 'css', 'munimap.css'),
-        ],
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.css/,
-        include: [
-          path.resolve(__dirname, 'src', 'css', 'example'),
-          path.join(__dirname, 'src', 'css', 'munimap.css'),
-        ],
-        type: 'asset/resource',
-        generator: {
-          filename: 'css/[name][ext][query]',
-        },
-      },
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.(png|jpg|jpeg|svg|ico)/,
-        exclude: /font/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'img/[name][ext][query]',
-        },
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        include: /font/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'font/[name][ext][query]',
-        },
-      },
-    ],
-  },
-  mode: 'development',
-  plugins: [
-    new webpack.DefinePlugin({
-      PRODUCTION: JSON.stringify(false),
-      VERSION: JSON.stringify(PACKAGE.version),
-      APP_PATH: JSON.stringify(APP_PATH),
-      PROD_DOMAIN: JSON.stringify(PROD_DOMAIN),
-    }),
-    new CopyPlugin({
-      patterns: [
+    },
+    module: {
+      rules: [
         {
-          from: '*.geojson',
-          to: 'example/data',
-          context: path.resolve(__dirname, 'resources'),
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+              options: {
+                minimize: false,
+                preprocessor: preprocessor,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          exclude: [
+            path.resolve(__dirname, 'src', 'css', 'example'),
+            path.join(__dirname, 'src', 'css', 'munimap.css'),
+          ],
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.css/,
+          include: [
+            path.resolve(__dirname, 'src', 'css', 'example'),
+            path.join(__dirname, 'src', 'css', 'munimap.css'),
+          ],
+          type: 'asset/resource',
+          generator: {
+            filename: 'css/[name][ext][query]',
+          },
+        },
+        {
+          test: /\.(js)$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+        {
+          test: /\.(png|jpg|jpeg|svg|ico)/,
+          exclude: /font/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'img/[name][ext][query]',
+          },
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          include: /font/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'font/[name][ext][query]',
+          },
         },
       ],
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-      ...opts,
-    }),
-    ...exampleHtmlPlugins,
-  ],
+    },
+    mode: 'development',
+    plugins: [
+      new webpack.DefinePlugin({
+        PRODUCTION: JSON.stringify(false),
+        VERSION: JSON.stringify(PACKAGE.version),
+        APP_PATH: JSON.stringify(APP_PATH),
+        PROD_DOMAIN: JSON.stringify(PROD_DOMAIN),
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: '*.geojson',
+            to: 'example/data',
+            context: path.resolve(__dirname, 'resources'),
+          },
+        ],
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: './index.html',
+        ...opts,
+      }),
+      ...exampleHtmlPlugins,
+    ],
+  };
 };
--- webpack.config.production.babel.js
+++ webpack.config.production.babel.js
@@ -5,140 +5,161 @@ import PACKAGE from './package.json';
 import glob from 'glob';
 import path from 'path';
 import webpack from 'webpack';
+import {CleanWebpackPlugin} from 'clean-webpack-plugin';
 
-const APP_PATH = '/munimap/latest/';
-const PROD_DOMAIN = 'maps.muni.cz';
+export default (env) => {
+  let dir;
+  let warningWrapperClass = '';
+  if (env.dir === 'latest' || env.dir === 'testing') {
+    dir = env.dir;
+    warningWrapperClass = 'hide';
+  } else if (env.dir === 'v2n') {
+    dir = PACKAGE.version.split('.').slice(0, 2).join('.');
+  } else {
+    throw new Error('Unknown directory for output files.');
+  }
 
-const OUTPUT_PATH = path.join(path.resolve(__dirname, 'dist'), APP_PATH);
+  const APP_PATH = `/munimap/${dir}/`;
+  const PROD_DOMAIN = 'maps.muni.cz';
+  const OUTPUT_PATH = path.join(path.resolve(__dirname, 'dist'), APP_PATH);
 
-const opts = {
-  scriptLoading: 'blocking',
-  inject: 'head',
-  minify: true,
-  appVersion: PACKAGE.version,
-  olVersion: PACKAGE.dependencies.ol.substr(0, 1).match(/[0-9]/i)
-    ? PACKAGE.dependencies.ol
-    : PACKAGE.dependencies.ol.substring(1),
-  appPath: APP_PATH,
-  prodDomain: PROD_DOMAIN,
-};
-const examplePageNames = glob
-  .sync('./src/example/*.html')
-  .map((item) => path.basename(item, '.html'));
-const exampleHtmlPlugins = examplePageNames.map((name) => {
-  return new HtmlWebpackPlugin({
-    template: `./src/example/${name}.html`,
-    filename: `./example/${name}.html`,
-    ...opts,
-  });
-});
-
-const preprocessor = (content) => {
-  const newContent = (content) => {
-    const INCLUDE_PATTERN = /<%= (.*) %>/gi;
-    return INCLUDE_PATTERN.test(content)
-      ? content.replace(INCLUDE_PATTERN, (_, group) => opts[group])
-      : content;
+  const opts = {
+    scriptLoading: 'blocking',
+    inject: 'head',
+    minify: true,
+    appVersion: PACKAGE.version,
+    olVersion: PACKAGE.dependencies.ol.substr(0, 1).match(/[0-9]/i)
+      ? PACKAGE.dependencies.ol
+      : PACKAGE.dependencies.ol.substring(1),
+    appPath: APP_PATH,
+    prodDomain: PROD_DOMAIN,
+    warningWrapperClass: warningWrapperClass,
   };
-  return newContent(content);
-};
 
-export default {
-  entry: {
-    munimaplib: [
-      'regenerator-runtime/runtime',
-      path.resolve(__dirname, 'src/munimap/index.js'),
-    ],
-  },
-  output: {
-    // path: path.resolve(__dirname, 'dist'),
-    path: OUTPUT_PATH,
-    filename: '[name].js',
-    library: {
-      name: 'munimap',
-      type: 'umd',
+  const examplePageNames = glob
+    .sync('./src/example/*.html')
+    .map((item) => path.basename(item, '.html'));
+
+  const exampleHtmlPlugins = examplePageNames.map((name) => {
+    return new HtmlWebpackPlugin({
+      template: `./src/example/${name}.html`,
+      filename: `./example/${name}.html`,
+      ...opts,
+    });
+  });
+
+  const preprocessor = (content) => {
+    const newContent = (content) => {
+      const INCLUDE_PATTERN = /<%= (.*) %>/gi;
+      return INCLUDE_PATTERN.test(content)
+        ? content.replace(INCLUDE_PATTERN, (_, group) => opts[group])
+        : content;
+    };
+    return newContent(content);
+  };
+
+  return {
+    entry: {
+      munimaplib: [
+        'regenerator-runtime/runtime',
+        path.resolve(__dirname, 'src/munimap/index.js'),
+      ],
     },
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: {
-              minimize: true,
-              preprocessor: preprocessor,
-            },
-          },
-        ],
+    output: {
+      path: OUTPUT_PATH,
+      filename: '[name].js',
+      library: {
+        name: 'munimap',
+        type: 'umd',
       },
-      {
-        test: /\.css$/,
-        exclude: [
-          path.resolve(__dirname, 'src', 'css', 'example'),
-          path.join(__dirname, 'src', 'css', 'munimap.css'),
-        ],
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.css/,
-        include: [
-          path.resolve(__dirname, 'src', 'css', 'example'),
-          path.join(__dirname, 'src', 'css', 'munimap.css'),
-        ],
-        type: 'asset/resource',
-        generator: {
-          filename: 'css/[name][ext][query]',
-        },
-      },
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.(png|jpg|jpeg|svg|ico)/,
-        exclude: /font/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'img/[name][ext][query]',
-        },
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        include: /font/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'font/[name][ext][query]',
-        },
-      },
-    ],
-  },
-  mode: 'production',
-  plugins: [
-    new webpack.DefinePlugin({
-      PRODUCTION: JSON.stringify(true),
-      VERSION: JSON.stringify(PACKAGE.version),
-      APP_PATH: JSON.stringify(APP_PATH),
-      PROD_DOMAIN: JSON.stringify(PROD_DOMAIN),
-    }),
-    new MiniCssExtractPlugin(),
-    new CopyPlugin({
-      patterns: [
+      clean: true,
+    },
+    module: {
+      rules: [
         {
-          from: '*.geojson',
-          to: 'example/data',
-          context: path.resolve(__dirname, 'resources'),
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+              options: {
+                minimize: true,
+                preprocessor: preprocessor,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          exclude: [
+            path.resolve(__dirname, 'src', 'css', 'example'),
+            path.join(__dirname, 'src', 'css', 'munimap.css'),
+          ],
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.css/,
+          include: [
+            path.resolve(__dirname, 'src', 'css', 'example'),
+            path.join(__dirname, 'src', 'css', 'munimap.css'),
+          ],
+          type: 'asset/resource',
+          generator: {
+            filename: 'css/[name][ext][query]',
+          },
+        },
+        {
+          test: /\.(js)$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+        {
+          test: /\.(png|jpg|jpeg|svg|ico)/,
+          exclude: /font/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'img/[name][ext][query]',
+          },
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          include: /font/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'font/[name][ext][query]',
+          },
         },
       ],
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-      ...opts,
-    }),
-    ...exampleHtmlPlugins,
-  ],
+    },
+    mode: 'production',
+    plugins: [
+      new webpack.DefinePlugin({
+        PRODUCTION: JSON.stringify(true),
+        VERSION: JSON.stringify(PACKAGE.version),
+        APP_PATH: JSON.stringify(APP_PATH),
+        PROD_DOMAIN: JSON.stringify(PROD_DOMAIN),
+      }),
+      new MiniCssExtractPlugin(),
+      new CleanWebpackPlugin({
+        dry: false,
+        cleanOnceBeforeBuildPatterns: [
+          path.join(path.resolve(__dirname, 'dist'), '**/*'),
+        ],
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: '*.geojson',
+            to: 'example/data',
+            context: path.resolve(__dirname, 'resources'),
+          },
+        ],
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: './index.html',
+        ...opts,
+      }),
+      ...exampleHtmlPlugins,
+    ],
+  };
 };
