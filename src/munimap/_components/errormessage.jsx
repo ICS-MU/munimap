42c13bbc524aebc77aa44871294a6bfa17acdeb6
Jan Tajovský <423920@mail.muni.cz>
2021-12-02 15:50:27 +0100

    moved error message helper functions to component file

--- src/munimap/_components/errormessage.jsx
+++ src/munimap/_components/errormessage.jsx
@@ -1,11 +1,63 @@
 import * as actions from '../redux/action.js';
+import * as munimap_lang from '../lang/lang.js';
 import * as slctr from '../redux/selector.js';
 import PropTypes from 'prop-types';
 import React, {useLayoutEffect, useRef} from 'react';
 import {ENABLE_EFFECT_LOGS, ENABLE_RENDER_LOGS} from '../conf.js';
-import {createInnerText, getErrorMessageStyle} from '../ui/interaction.js';
 import {hot} from 'react-hot-loader';
 import {useDispatch, useSelector} from 'react-redux';
+
+/**
+ * @typedef {import("ol/Map").default} ol.Map
+ */
+
+/**
+ * @typedef {Object} InvalidCodeOptions
+ * @property {Array<string>} invalidCodes invalid codes
+ * @property {string} lang language
+ * @property {ol.Map} map map
+ */
+
+/**
+ * @param {HTMLElement} errEl element
+ * @return {{size: number, lineHeight: number}} result
+ */
+const getErrorMessageStyle = (errEl) => {
+  const dpr = window.devicePixelRatio || 1;
+  let size;
+  let lineHeight;
+  if (errEl.offsetWidth < 500) {
+    size = 22 * dpr;
+    lineHeight = 26 * dpr;
+  } else {
+    size = 30 * dpr;
+    lineHeight = 35 * dpr;
+  }
+  return {size, lineHeight};
+};
+
+/**
+ * @param {Array<string>} invalidCodes invalid codes
+ * @param {boolean} simpleScroll simple scroll
+ * @param {string} lang language
+ * @return {string|undefined} message
+ */
+const createInnerText = (invalidCodes, simpleScroll, lang) => {
+  const hasInvalidCodes = invalidCodes && invalidCodes.length > 0;
+  const shouldBlockMap = !simpleScroll;
+  let msg;
+  if (hasInvalidCodes) {
+    msg =
+      munimap_lang.getMsg(munimap_lang.Translations.ACTIVATE_MAP, lang) +
+      '\n' +
+      munimap_lang.getMsg(munimap_lang.Translations.NOT_FOUND, lang) +
+      ':\n' +
+      invalidCodes.join(', ');
+  } else if (shouldBlockMap) {
+    msg = munimap_lang.getMsg(munimap_lang.Translations.ACTIVATE_MAP, lang);
+  }
+  return msg;
+};
 
 /**
  * @type {React.FC<{
