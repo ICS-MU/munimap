6f4f59624b8c87281b8d9c2c2b9f39ef0b12c922
Jan Tajovský <423920@mail.muni.cz>
2022-05-12 12:52:33 +0200

    fixel loading message zindex

--- src/munimap/components/loadingmessage.jsx
+++ src/munimap/components/loadingmessage.jsx
@@ -23,7 +23,7 @@ const LoadingMessageComponent = (props) => {
           ' font-weight: bold; vertical-align: middle; ' +
           ' font-family: Arial, Helvetica, sans-serif; ' +
           ' position: absolute; top: 0; left: 0; width: 100%;' +
-          ' height: 100%; text-align: center;'
+          ' height: 100%; text-align: center; z-index: 1;'
       );
     }
 
