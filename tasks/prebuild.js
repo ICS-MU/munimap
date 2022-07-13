aaf8e4984225277aada306e389abe9eb4beadf9e
Jan Tajovský <423920@mail.muni.cz>
2022-07-13 13:53:08 +0200

    added info about license to output js file

---
+++ tasks/prebuild.js
@@ -0,0 +1,22 @@
+import git from 'git-state'
+
+const checkRepoState = () => {
+  const state = git.checkSync('.');
+  let msg;
+  if(state.branch !== 'master') {
+    msg = 'You are not on "master" branch';
+  } else if (isNaN(state.ahead)) {
+    msg = 'No "remote" set for local repository';
+  } else if (state.ahead > 0) {
+    msg = 'You are ahead of remote';
+  } else if (state.dirty > 0 || state.untracked > 0) {
+    msg = 'Some dirty or untracked files in your repo ' + 
+        '(untracked, modified, deleted, etc.)';
+  }
+  if(msg) {
+    console.log(state);
+    // throw new Error(msg);
+  }
+}
+
+checkRepoState();
