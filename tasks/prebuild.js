a230b711643059b457e448eb1989551f66788705
Jan Tajovský <423920@mail.muni.cz>
2022-07-15 14:24:29 +0200

    prebuild task refactoring

--- tasks/prebuild.js
+++ tasks/prebuild.js
@@ -1,22 +1,25 @@
-import git from 'git-state'
+/* eslint-disable no-console */
+
+import git from 'git-state';
 
 const checkRepoState = () => {
   const state = git.checkSync('.');
   let msg;
-  if(state.branch !== 'master') {
-    msg = 'You are not on "master" branch';
+  if (state.branch !== 'master') {
+    msg = 'You are not on "master" branch!';
   } else if (isNaN(state.ahead)) {
-    msg = 'No "remote" set for local repository';
+    msg = 'No "remote" set for local repository!';
   } else if (state.ahead > 0) {
-    msg = 'You are ahead of remote';
+    msg = 'You are ahead of remote!';
   } else if (state.dirty > 0 || state.untracked > 0) {
-    msg = 'Some dirty or untracked files in your repo ' + 
-        '(untracked, modified, deleted, etc.)';
+    msg =
+      'Some dirty or untracked files in your repo ' +
+      '(untracked, modified, deleted, etc.).';
   }
-  if(msg) {
-    console.log(state);
-    // throw new Error(msg);
+  if (msg) {
+    console.log(msg, '\n', state, '\n\n');
+    throw new Error(msg);
   }
-}
+};
 
 checkRepoState();
