0b0073b3764c08533aa8d11b4cf3a8fe7db1243e
Jan Tajovský <423920@mail.muni.cz>
2022-08-18 15:02:17 +0200

    exported new functions from munimap

--- src/munimap/index.js
+++ src/munimap/index.js
@@ -10,25 +10,32 @@ import 'ol/ol.css';
 import '../css/munimaplib.css'; //must be after ol.css => rewrite some rules
 
 import * as mm_matomo from './matomo.js';
+import ClusterSource from 'ol/source/Cluster.js';
+import EnhancedVectorSource from './source/vector.js';
 import VectorSource from 'ol/source/Vector.js';
 import create from './create.js';
 import reset from './reset.js';
 import {Attribution, Control, FullScreen, Zoom} from 'ol/control.js';
+import {Circle, Fill, Icon, Stroke, Style, Text} from 'ol/style.js';
+import {EnhancedClusterSource} from './source/cluster.js';
 import {Feature, Map, View} from 'ol';
-import {GeoJSON} from 'ol/format.js';
+import {GeoJSON, WKT} from 'ol/format.js';
 import {Point} from 'ol/geom.js';
-import {Stroke, Style} from 'ol/style.js';
 import {Tile, Vector} from 'ol/layer.js';
 import {createSelector} from 'reselect';
 import {createXYZ} from 'ol/tilegrid.js';
 import {featuresForMap} from './load/load.js';
 import {
   getActiveFloorCodes,
+  getExtent,
   getSelectedFloorCode,
+  isInFloorResolutionRange,
 } from './redux/selector/selector.js';
 import {getNotYetAddedFeatures} from './utils/store.js';
 import {getStoreByTargetId} from './constants.js';
+import {selected_feature_changed} from './redux/action.js';
 import {tile} from 'ol/loadingstrategy.js';
+import {unByKey} from 'ol/Observable.js';
 
 import '../img/rectangle.png';
 import '../img/rectangle_blue.png';
@@ -58,7 +65,10 @@ const ol = {
     Zoom,
   },
   source: {
+    Cluster: ClusterSource,
     Vector: VectorSource,
+    EnhancedCluster: EnhancedClusterSource,
+    EnhancedVector: EnhancedVectorSource,
   },
   loadingstrategy: {
     tile,
@@ -67,18 +77,32 @@ const ol = {
     createXYZ,
   },
   style: {
+    Circle,
+    Fill,
+    Icon,
     Stroke,
     Style,
+    Text,
   },
   format: {
     GeoJSON,
+    WKT,
+  },
+  Observable: {
+    unByKey,
   },
 };
 
 const slctr = {
   createSelector,
   getActiveFloorCodes,
+  getExtent,
   getSelectedFloorCode,
+  isInFloorResolutionRange,
+};
+
+const action = {
+  selected_feature_changed,
 };
 
 const load = {
@@ -99,4 +123,4 @@ if (!window.hasOwnProperty('ol')) {
   );
 }
 
-export {create, reset, ol, slctr, load, store, getStoreByTargetId};
+export {create, reset, ol, action, slctr, load, store, getStoreByTargetId};
